4b4bcec320e828258e1372c2f1d5e4da183d8659
김서규 <seogyu.kim@kbfg.com>
2026-01-23 15:33:41 +0900

    feat: Support nested tables in TableCell

--- src/document/table_cell.rs
+++ src/document/table_cell.rs
@@ -4,7 +4,10 @@ use std::borrow::{Borrow, Cow};
 use derive_more::From;
 use hard_xml::{XmlRead, XmlWrite};
 
-use crate::{__setter, __xml_test_suites, document::Paragraph, formatting::TableCellProperty};
+use crate::{
+    __setter, __xml_test_suites, document::Paragraph, document::Table,
+    formatting::TableCellProperty,
+};
 
 /// Table Cell
 ///
@@ -23,7 +26,7 @@ use crate::{__setter, __xml_test_suites, document::Paragraph, formatting::TableC
 pub struct TableCell<'a> {
     #[xml(default, child = "w:tcPr")]
     pub property: TableCellProperty,
-    #[xml(child = "w:p")]
+    #[xml(child = "w:p", child = "w:tbl")]
     pub content: Vec<TableCellContent<'a>>,
 }
 
@@ -37,16 +40,26 @@ impl<'a> TableCell<'a> {
         }
     }
 
-    pub fn iter_text(&self) -> impl Iterator<Item = &Cow<'a, str>> {
-        self.content.iter().flat_map(|content| match content {
-            TableCellContent::Paragraph(p) => p.iter_text(),
-        })
+    pub fn iter_text(&self) -> Box<dyn Iterator<Item = &Cow<'a, str>> + '_> {
+        Box::new(self.content.iter().flat_map(|content| match content {
+            TableCellContent::Paragraph(p) => {
+                Box::new(p.iter_text()) as Box<dyn Iterator<Item = &Cow<'a, str>>>
+            }
+            TableCellContent::Table(t) => {
+                Box::new(t.iter_text()) as Box<dyn Iterator<Item = &Cow<'a, str>>>
+            }
+        }))
     }
 
-    pub fn iter_text_mut(&mut self) -> impl Iterator<Item = &mut Cow<'a, str>> {
-        self.content.iter_mut().flat_map(|content| match content {
-            TableCellContent::Paragraph(p) => p.iter_text_mut(),
-        })
+    pub fn iter_text_mut(&mut self) -> Box<dyn Iterator<Item = &mut Cow<'a, str>> + '_> {
+        Box::new(self.content.iter_mut().flat_map(|content| match content {
+            TableCellContent::Paragraph(p) => {
+                Box::new(p.iter_text_mut()) as Box<dyn Iterator<Item = &mut Cow<'a, str>>>
+            }
+            TableCellContent::Table(t) => {
+                Box::new(t.iter_text_mut()) as Box<dyn Iterator<Item = &mut Cow<'a, str>>>
+            }
+        }))
     }
 
     pub fn replace_text<'b, I, T, S>(&mut self, dic: T) -> crate::DocxResult<()>
@@ -58,6 +71,7 @@ impl<'a> TableCell<'a> {
         for content in self.content.iter_mut() {
             match content {
                 TableCellContent::Paragraph(p) => p.replace_text(dic)?,
+                TableCellContent::Table(t) => t.replace_text(dic)?,
             }
         }
         Ok(())
@@ -78,12 +92,14 @@ impl<'a, T: Into<TableCellContent<'a>>> From<T> for TableCell<'a> {
 pub enum TableCellContent<'a> {
     #[xml(tag = "w:p")]
     Paragraph(Paragraph<'a>),
-    // #[xml(tag = "w:tbl")]
-    // Table(Table<'a>),
+    #[xml(tag = "w:tbl")]
+    Table(Table<'a>),
 }
 
 __xml_test_suites!(
     TableCell,
     TableCell::paragraph(Paragraph::default()),
     r#"<w:tc><w:tcPr><w:vAlign w:val="top"/></w:tcPr><w:p/></w:tc>"#,
+    TableCell::from(Table::default()),
+    r#"<w:tc><w:tcPr><w:vAlign w:val="top"/></w:tcPr><w:tbl><w:tblPr/><w:tblGrid/></w:tbl></w:tc>"#,
 );
